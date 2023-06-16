3b7335a61769b166395cf0abd74fc7190537121f
pushvinderrohtagi <pushvinder.rohta@tigeranalytics.com>
2023-06-16 22:33:26 +0530

    creating package

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ from distutils.core import setup  # isort: skip
 
 ROOT = op.dirname(op.abspath(__file__))
 SRC = op.join(ROOT, "src")
+print(ROOT)
 
 
 def get_install_req():
-    with io.open("deploy/requirements.txt") as fh:
+    with io.open(ROOT + "/deploy/requirements.txt") as fh:
         install_reqs = fh.read()
     install_reqs = [l for l in install_reqs.split("\n") if len(l) > 1]
     return install_reqs
@@ -23,7 +24,7 @@ install_reqs = get_install_req()
 def get_version_info():
     """Extract version information as a dictionary from version.py."""
     version_info = {}
-    version_filename = os.path.join("src", "version.py")
+    version_filename = os.path.join(ROOT, "src", "version.py")
     with open(version_filename, "r") as version_module:
         version_code = compile(version_module.read(), "version.py", "exec")
     exec(version_code, version_info)
